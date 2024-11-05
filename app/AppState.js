37ac117feb929427752c8de37382737aae2000dd
heather bickish <hbickish@gmail.com>
2024-11-05 16:35:05 -0700

    created draw snacks function in controller

--- app/AppState.js
+++ app/AppState.js
@@ -9,17 +9,20 @@ class ObservableAppState extends EventEmitter {
 
     new Snack({
       name: 'Popcorn',
-      price: 1.25
+      price: 1.25,
+      img: 'https://images.unsplash.com/photo-1512149074996-e923ac45be6a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHNuYWNrc3xlbnwwfHwwfHx8MA%3D%3D '
     }),
 
     new Snack({
       name: 'Burrito',
-      price: 3
+      price: 3,
+      img: 'https://images.unsplash.com/photo-1662116765994-1e4200c43589?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnVycml0b3xlbnwwfHwwfHx8MA%3D%3D'
     }),
 
     new Snack({
       name: 'Fish Sticks',
-      price: 2.75
+      price: 2.75,
+      img: 'https://plus.unsplash.com/premium_photo-1712932551568-91205970a69e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZpc2glMjBzdGlja3N8ZW58MHx8MHx8fDA%3D'
     }),
   ]
 
--- app/controllers/SnackController.js
+++ app/controllers/SnackController.js
@@ -1,10 +1,18 @@
+import { AppState } from "../AppState.js"
+
 export class SnackController {
   constructor() {
     console.log('Snack Controller is loaded')
   }
 
 
-
+  drawSnacks() {
+    const snacks = AppState.snacks
+    let snackImg = ''
+    snacks.forEach(snack => snackImg += snack.imgUrl)
+    const snackCatalogElm = document.getElementById('snack-catalog')
+    snackCatalogElm.innerHTML = snackCatalogElm.toString()
+  }
 
 
 }
